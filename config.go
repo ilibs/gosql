a8458bdcfbe385806828364a083777574055e38b
fifsky <fifsky@gmail.com>
2020-04-02 14:52:54 +0800

    config struct tag support yml

--- config.go
+++ config.go
@@ -1,12 +1,12 @@
 package gosql
 
-//Config is database connection configuration
+// Config is database connection configuration
 type Config struct {
-	Enable       bool   `toml:"enable" json:"enable"`
-	Driver       string `toml:"driver" json:"driver"`
-	Dsn          string `toml:"dsn" json:"dsn"`
-	MaxOpenConns int    `toml:"max_open_conns" json:"max_open_conns"`
-	MaxIdleConns int    `toml:"max_idle_conns" json:"max_idle_conns"`
-	MaxLifetime  int    `toml:"max_lefttime" json:"max_lefttime"`
-	ShowSql      bool   `toml:"show_sql" json:"show_sql"`
+	Enable       bool   `yml:"enable" toml:"enable" json:"enable"`
+	Driver       string `yml:"driver" toml:"driver" json:"driver"`
+	Dsn          string `yml:"dsn" toml:"dsn" json:"dsn"`
+	MaxOpenConns int    `yml:"max_open_conns" toml:"max_open_conns" json:"max_open_conns"`
+	MaxIdleConns int    `yml:"max_idle_conns" toml:"max_idle_conns" json:"max_idle_conns"`
+	MaxLifetime  int    `yml:"max_lefttime" toml:"max_lefttime" json:"max_lefttime"`
+	ShowSql      bool   `yml:"show_sql" toml:"show_sql" json:"show_sql"`
 }
